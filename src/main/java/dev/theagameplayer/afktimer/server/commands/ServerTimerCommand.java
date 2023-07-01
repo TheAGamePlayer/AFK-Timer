d49db2a70cefa7307a121ded069bad5574544136
TheAGamePlayer <79672176+TheAGamePlayer@users.noreply.github.com>
2023-07-01 00:46:22 -0700

    See 2.0.1.0R Changelog

--- src/main/java/dev/theagameplayer/afktimer/server/commands/ServerTimerCommand.java
+++ src/main/java/dev/theagameplayer/afktimer/server/commands/ServerTimerCommand.java
@@ -19,19 +19,19 @@ public final class ServerTimerCommand {
 				}).then(Commands.literal("start").then(Commands.argument("time", TimeArgument.time()).executes(ctx -> {
 					AFKServerEvents.serverTime = IntegerArgumentType.getInteger(ctx, "time");
 					AFKServerEvents.serverActive = true;
-					ctx.getSource().sendSuccess(Component.translatable("commands.afktimer.server.start", IntegerArgumentType.getInteger(ctx, "time")).withStyle(Style.EMPTY.withColor(ChatFormatting.DARK_GRAY)), true);
+					ctx.getSource().sendSuccess(() -> Component.translatable("commands.afktimer.server.start", IntegerArgumentType.getInteger(ctx, "time")).withStyle(Style.EMPTY.withColor(ChatFormatting.DARK_GRAY)), true);
 					return 0;
 				}))).then(Commands.literal("stop").executes(ctx -> {
 					AFKServerEvents.serverActive = false;
 					AFKServerEvents.serverTick = 0;
-					ctx.getSource().sendSuccess(Component.translatable("commands.afktimer.server.stop").withStyle(Style.EMPTY.withColor(ChatFormatting.DARK_GRAY)), true);
+					ctx.getSource().sendSuccess(() -> Component.translatable("commands.afktimer.server.stop").withStyle(Style.EMPTY.withColor(ChatFormatting.DARK_GRAY)), true);
 					return 0;
 				})).then(Commands.literal("query").then(Commands.literal("time").executes(ctx -> {
-					ctx.getSource().sendSuccess(Component.translatable("commands.afktimer.server.query", AFKServerEvents.serverTime - AFKServerEvents.serverTick).withStyle(Style.EMPTY.withColor(ChatFormatting.DARK_GRAY)), false);
+					ctx.getSource().sendSuccess(() -> Component.translatable("commands.afktimer.server.query", AFKServerEvents.serverTime - AFKServerEvents.serverTick).withStyle(Style.EMPTY.withColor(ChatFormatting.DARK_GRAY)), false);
 					return 0;
 				}))).then(Commands.literal("extend").then(Commands.argument("time", TimeArgument.time()).executes(ctx -> {
 					AFKServerEvents.serverTime += IntegerArgumentType.getInteger(ctx, "time");
-					ctx.getSource().sendSuccess(Component.translatable("commands.afktimer.server.extend", IntegerArgumentType.getInteger(ctx, "time")).withStyle(Style.EMPTY.withColor(ChatFormatting.DARK_GRAY)), true);
+					ctx.getSource().sendSuccess(() -> Component.translatable("commands.afktimer.server.extend", IntegerArgumentType.getInteger(ctx, "time")).withStyle(Style.EMPTY.withColor(ChatFormatting.DARK_GRAY)), true);
 					return 0;
 				})));
 	}
